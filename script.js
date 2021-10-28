7a85c979b84b0e0eba3b7bf29e3d9ae4940c5c34
Dima Tsymbal <dima.tsymbal12345@gmail.com>
2021-10-28 19:30:40 +0300

    Task 5 completed

---
+++ script.js
@@ -0,0 +1,19 @@
+const taskFive = 5
+console.log('Task ' + taskFive)
+
+let a = 5
+let b = 5
+console.log(a + b)
+
+let c = 141
+let d = 75
+console.log(c - d)
+
+let e = 9
+let f = 8
+console.log(e*f)
+
+let g = 81
+let h = 9
+console.log(g / h)
+//-------------------------
